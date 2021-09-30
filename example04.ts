41f4db3cf2dc81a82cba56d5aa7b49ee8ea99768
gabriel-roque <gabrielrms.dll@gmail.com>
2021-09-30 19:25:26 -0300

    more examples

---
+++ example04.ts
@@ -0,0 +1,36 @@
+/* 
+  Pipe's de write stream pode causar vazamento de memória, e como podemos ter vários pipe
+  eventualmente algum pode causar erro e não ter uma tratativa adequada
+*/
+
+import { pipeline, Readable, Writable } from 'stream'
+import { promisify } from 'util';
+
+const pipelineAsync = promisify(pipeline)
+
+const readableStream = new Readable({
+  read: function () {
+    this.push('Hello Dude!! 0')
+    this.push('Hello Dude!! 1')
+    this.push('Hello Dude!! 2')
+    this.push(null)
+  }
+})
+
+const writableStream = new Writable({
+  write(chunk, encoding, cb) {
+    console.log('chunk >>>', chunk, Buffer.from(chunk).toString('utf-8'));
+    cb()
+  }
+})
+
+async function run() {
+  await pipelineAsync(
+    readableStream,
+    writableStream
+    // process.stdout
+  )
+}
+
+run()
+
---
+++ example05.ts
@@ -0,0 +1,60 @@
+import { pipeline, Readable, Writable, Transform } from 'stream'
+import { promisify } from 'util'
+import { createWriteStream } from 'fs';
+
+const pipelineAsync = promisify(pipeline)
+
+const readableStream = new Readable({
+  read: function () {
+    for (let index = 0; index < 1e5; index++) {
+      const person = { id: Date.now() + index, name: `John Due-${index}` }
+      const data = JSON.stringify(person)
+      this.push(data)
+    }
+    this.push(null)
+  }
+})
+
+const writableMaptoCSV = new Transform({
+  transform: (chunk, encondig, cb) => {
+    const data = JSON.parse(chunk)
+    const result = `${data.id},${String(data.name).toUpperCase()}\n`
+
+    cb(null, result)
+  }
+})
+
+let counter: number
+const setHeader = new Transform({
+  transform: (chunk, encoding, cb) => {
+    counter = counter ?? 0
+    if (counter) {
+      return cb(null, chunk)
+    }
+
+    counter++
+
+    cb(null, "id,name\n".concat(chunk))
+  }
+})
+
+
+const writableStream = new Writable({
+  write: (chunk, encoding, cb) => {
+    console.log('chunk >>>', chunk, Buffer.from(chunk).toString('utf-8'));
+    cb()
+  }
+})
+
+
+async function run() {
+  await pipelineAsync(
+    readableStream,
+    writableMaptoCSV,
+    setHeader,
+    // writableStream,
+    createWriteStream('my.csv')
+  ).finally(() => console.log('Finish Strem!'))
+}
+
+run()
