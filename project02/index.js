03b3be78446b76fa1092a5a979f5ec74d2a510a0
gabriel-roque <gabrielrms.dll@gmail.com>
2021-09-30 22:38:09 -0300

    example large processing stram

---
+++ project02/index.js
@@ -0,0 +1,54 @@
+import { dirname, join } from 'path'
+import { promisify } from 'util'
+import { promises, createReadStream, createWriteStream } from 'fs';
+import { pipeline, Transform } from 'stream';
+import csvtojson from 'csvtojson';
+import jsontocsv from 'json-to-csv-stream';
+import StreamConcat from 'stream-concat';
+
+const pipelineAync = promisify(pipeline)
+const { readdir } = promises
+import debug from 'debug';
+const log = debug('app:concat')
+
+const { pathname: currentFile } = new URL(import.meta.url)
+
+const cwd = dirname(currentFile)
+const fileDir = `${cwd}/dataset`
+const output = `${cwd}/final.csv`
+
+console.time('concat-data');
+const files = (await readdir(fileDir))
+  .filter(item => !(!!~item.indexOf('.zip')))
+
+console.log(`processing ${files}`)
+const ONE_SECOND = 1000
+// quando os outro processos acabarem ele morre junto usando o .unref()
+setInterval(() => process.stdout.write('.'), ONE_SECOND).unref()
+
+// const combinedStreams = createReadStream(join(fileDir, files[0]))
+const streams = files.map(item => createReadStream(join(fileDir, item)))
+const combinedStreams = new StreamConcat(streams)
+
+const finalSream = createWriteStream(output)
+const handleStream = new Transform({
+  transform: (chunk, encoding, cb) => {
+    const data = JSON.parse(chunk)
+    const output = {
+      id: data.Respondent,
+      country: data.Country
+    }
+    // console.log(`ID: ${output.id}`);
+    return cb(null, JSON.stringify(output))
+  }
+})
+
+await pipelineAync(
+  combinedStreams,
+  csvtojson(),
+  handleStream,
+  jsontocsv(),
+  finalSream
+)
+console.log(`${files.length} files merged! on ${output}`)
+console.timeEnd('concat-data')
